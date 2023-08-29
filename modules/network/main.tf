4eebe4314b39a40c1cdee70bee9541929cecda44
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-29 19:35:28 -0300

    bastion subnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -6,6 +6,54 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = var.group
 }
 
+### Bastion Subnet ###
+resource "azurerm_network_security_group" "bastion" {
+  name                = "nsg-${var.sys}-bastion"
+  location            = var.location
+  resource_group_name = var.group
+}
+
+resource "azurerm_network_security_rule" "inbound" {
+  name                        = "BastionInbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.group
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
+
+resource "azurerm_network_security_rule" "bastion" {
+  name                        = "BastionOutbound"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.group
+  network_security_group_name = azurerm_network_security_group.bastion.name
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "bastion-subnet"
+  resource_group_name  = var.group
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.10.0/24"]
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "bastion" {
+  subnet_id                 = azurerm_subnet.bastion.id
+  network_security_group_id = azurerm_network_security_group.bastion.id
+}
+
 ### Linux Subnet ###
 resource "azurerm_network_security_group" "linux" {
   name                = "nsg-${var.sys}-linux"
@@ -45,7 +93,7 @@ resource "azurerm_subnet" "linux" {
   name                 = "linux-subnet"
   resource_group_name  = var.group
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.10.0/24"]
+  address_prefixes     = ["10.0.50.0/24"]
 }
 
 
--- modules/network/output.tf
+++ modules/network/output.tf
@@ -2,6 +2,10 @@ output "vnet_id" {
   value = azurerm_virtual_network.main.id
 }
 
+output "bastion_subnet_id" {
+  value = azurerm_subnet.bastion.id
+}
+
 output "linux_subnet_id" {
   value = azurerm_subnet.linux.id
 }
